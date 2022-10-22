dc07051e4a6ce50c58f52f9ef97f4d40af7ed981
Ngan Do <dongocthanhngan2604@gmail.com>
2022-10-22 23:37:35 +0700

    feature: add google translate

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Hello")
+async function main() {
+  const translate = require("@vitalets/google-translate-api");
+
+  const res = await translate("Tôi nói tiếng việt không rành", { to: "en" });
+
+  console.log(res.text); //=> I speak English
+  console.log(res.from.language.iso); //=> nl
+}
+
+main();
